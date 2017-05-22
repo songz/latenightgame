d56e4774e57a5d7ea66b7cc8b42802714c9aa614
Song Zheng <song@hackerville.io>
2017-05-21 23:21:33 -0700

    ENLIST

--- routes/song.js
+++ routes/song.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,8 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/intro', function(req, res, next) {
-  res.json({title: 'hello', description: 'Hello, welcome to this game. Please send another request every minute to this url for new instructions until further notice. You are getting deployed! Send a get request to /step1'});
+router.get('/next', function(req, res, next) {
+  res.json({title: 'hello', description: 'please check back in 5 minutes'});
 });
 
 router.get('/step1', function(req, res, next) {
@@ -19,19 +20,20 @@ router.post('/test', function(req, res, next) {
   const postData = req.body;
   let message = "WRONG, try again...";
   if(postData.myTag) {
-    message = 'CONGRATS! you have passed test #1! But mothership is ALMOST ready!.... check back in 5 minutes...'
-    console.log(postData.myTag);
+    message = 'CONGRATS! you have passed the test ! You are ready to be deployed! To start, you have to enlist yourself. To create information, you must send a POST request to /enlist with your name. Sample data: {name: "Rahul the godly Brownie"}. To see if you have been enlisted, send a GET request to /enlist and you will see people who are enlisted.';
   }
   res.json({title: 'STEP1', description: message});
 });
 
 router.post('/enlist', function(req, res, next) {
   const postData = req.body;
-  let message = "WRONG, try again...";
-  if(postData.myTag) {
-    message = 'CONGRATS! you have passed the test ! You are ready to be deployed! To start, you have to enlist yourself. To create information, you must send a POST request to /enlist with your name. Sample data: {name: "Rahul the godly Brownie"}. To see if you have been enlisted, send a GET request to /enlist and you will see people who are enlisted.';
+  if(postData.name) {
+    var enlistKey = firebase.database().ref('enlist').push(postData);
+    console.log("ENLIST SUCCESS FOR ", postData.name);
+    res.json({title: 'STEP2', description: `Congratulations! You have been enlisted. Your enlistment ID is: ${enlistKey.key}. Please keep this id safe, you will need this for ALL future requests. send a GET request to /next/enlistID to get next instructions `});
+  } else {
+    res.json({title: 'ERROR', description: 'your name does not exist in your request'});
   }
-  res.json({title: 'STEP1', description: message});
 });
 
 module.exports = router;
--- routes/vig.js
+++ routes/vig.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
+
 /* GET home page. */
 router.post('/enlist', function(req, res, next) {
   var enlistKey = firebase.database.ref('enlist').push(enlistData);
@@ -9,9 +10,12 @@ router.post('/enlist', function(req, res, next) {
 });
 
 router.post('/enlist', function(req, res, next) {
-  var enlistKey = firebase.database.ref('enlist').push(enlistData);
+  const enlistData = req.body;
+  console.log('what data?', enlistData);
+  var enlistKey = firebase.database().ref('enlist').push(enlistData);
+  console.log('enlistKey', enlistKey);
 
-  res.json({dataId: enlistKey});
+  res.json({title: 'STEP1', description: message});
 });
 
 module.exports = router;
