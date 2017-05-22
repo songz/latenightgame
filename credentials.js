5547296f4c6271130a04d6cbd379fdce9879ca4c
Yoo Jin Lim <yjlim5@gmail.com>
2017-05-21 22:17:05 -0700

    add firebase credentials and gitignore files

---
+++ credentials.js
@@ -0,0 +1,8 @@
+let config = {
+  apiKey: "AIzaSyBYa1yFNdNMuouHHYzZoJ9kRO26TnEOmCg",
+  authDomain: "latenightgame-969b3.firebaseapp.com",
+  databaseURL: "https://latenightgame-969b3.firebaseio.com",
+  projectId: "latenightgame-969b3",
+  storageBucket: "latenightgame-969b3.appspot.com",
+  messagingSenderId: "440451556696"
+};
